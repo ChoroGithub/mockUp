ab29be9ef5ec00d08a3d97a4858a6d00fbc14338
Choro <tschoro.ulan.uulu@gmail.com>
2021-06-29 20:05:16 +0200

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 
@@ -10,5 +11,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+ 
 
 }
